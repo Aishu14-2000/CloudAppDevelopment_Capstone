a234b1b0f99583fa70906ffded9ec78bbfbec9e0
Aishu14-2000 <rnarmadha14@gmail.com>
2022-08-23 13:23:26 +0000

    changes

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,9 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost","berose7.us-south.cf.appdomain.cloud"]
+ALLOWED_HOSTS = ["localhost","localhost:8000","berose7.us-south.cf.appdomain.cloud","https://rnarmadha14-8000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
+
+CSRF_TRUSTED_ORIGINS = ['https://rnarmadha14-8000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 
 # Application definition
